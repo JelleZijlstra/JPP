b47b88cfd99ccdcbe75531a33635981a839d4600
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2012-12-08 18:21:00 -0500

    Start work on website

---
+++ html/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Some parts of this website are loosely based on the nascent CS50 Framework: http://cdn.cs50.net/2012/fall/psets/7/pset7.zip -->
+    <meta http-equiv="Content-Type" charset="text/html; charset=UTF-8"/>
+
+    <link href="css/bootstrap.css" rel="stylesheet"/>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet"/>
+    <link href="css/styles.css" rel="stylesheet"/>
+	<title>JPP: Jelle's Phylogeny Program</title>
+    <script src="js/jquery-1.8.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/scripts.js"></script>
+</head>
+<body>
+
+<h1>Jelle's Phylogeny Program</h1>
+
+</body>
+</html>
