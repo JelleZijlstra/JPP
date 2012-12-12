5510a604f83bde1cd30df5c209a304bcda80455c
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2012-12-12 15:19:33 -0500

    Text tweak

--- html/templates/home.php
+++ html/templates/home.php
@@ -4,7 +4,7 @@
 <div class="centered-image">
 	<img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Darwin_tree_cut.png" alt="An early phylogenetic tree drawn by Darwin"/>
 </div>
-<p>The above image shows the first evolutionary tree drawn by Charles Darwin in 1837. It shows the relationships among a set of arbitrary species in the form of a tree. The image of a tree is still used to represent the relationships among different organisms, and since the latter half of the 20th century it has become common to use numerical methods to reconstruct evolutionary trees algorithmically. Maximum parsimony is one method used for this purpose (others are maximum likelihood, Bayesian inference, and neighbor-joining).</p>
+<p>The above image is the first evolutionary tree drawn by Charles Darwin in 1837. It shows the relationships among a set of arbitrary species in the form of a tree. The image of a tree is still used to represent the relationships among different organisms, and since the latter half of the 20th century it has become common to use numerical methods to reconstruct evolutionary trees algorithmically. Maximum parsimony is one method used for this purpose (others are maximum likelihood, Bayesian inference, and neighbor-joining).</p>
 
 <p>I worked on this project as part of <a href="http://www.cs205.org">Computer Science 205</a>: Computing Foundations for Computational Science. In this website, I provide documentation and background for the project, including the following:</p>
 <ul>
